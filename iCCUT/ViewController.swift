af4779cb47043bf06035e4cf286fb8d4721c5c89
Maru-zhang <475435200@qq.com>
2015-09-19 11:27:08 +0800

    Initial Commit

---
+++ iCCUT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iCCUT
+//
+//  Created by Maru on 15/9/19.
+//  Copyright © 2015年 Alloc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
