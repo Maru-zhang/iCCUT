43011470e303fb88a48e9a1c90a2ecf78a0174c4
Maru-zhang <maru-zhang@foxmail.com>
2016-05-27 23:27:55 +0800

    解决了搜索控制器的显示BUG；

--- iCCUT/Module/Media/Controller/CCVideoSearchController.swift
+++ iCCUT/Module/Media/Controller/CCVideoSearchController.swift
@@ -8,21 +8,18 @@
 
 import Cartography
 
-class CCVideoSearchController: UITableViewController,UISearchBarDelegate,UISearchResultsUpdating,UISearchControllerDelegate {
+class CCVideoSearchController: UITableViewController {
     
     var resultVC: UITableViewController!
-    let searchController = UISearchController(searchResultsController: UITableViewController(style: .Plain))
+    var searchController: UISearchController!
 
-    /// 唯一标识
-    let identifier = "searchCell"
     /// 数据源
     var dataSource = [CCVideoModel]()
     
     // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        setupSetting()
         
         setupView()
     }
@@ -30,26 +27,32 @@ class CCVideoSearchController: UITableViewController,UISearchBarDelegate,UISearc
     // MARK: - Private Method
     private func setupView() {
         
-        self.tableView.registerClass(UITableViewCell.classForCoder(), forCellReuseIdentifier: identifier)
-        
+        resultVC = UITableViewController(style: .Plain)
+        resultVC.automaticallyAdjustsScrollViewInsets = false
+        resultVC.tableView.contentInset = UIEdgeInsetsMake(54, 0, 0, 0)
+        searchController = UISearchController(searchResultsController: resultVC)
+                
         tableView.tableHeaderView = searchController.searchBar
         
         searchController.searchBar.sizeToFit()
         searchController.searchBar.tintColor = UIColor.whiteColor()
         searchController.searchBar.placeholder = "请输入视频名称/关键字"
+        searchController.dimsBackgroundDuringPresentation = true
         searchController.delegate = self
         searchController.searchResultsUpdater = self
         searchController.searchBar.delegate = self
         
-        resultVC = searchController.searchResultsController as! UITableViewController
+        resultVC.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: String(UITableViewCell))
         resultVC.tableView.delegate = self
         resultVC.tableView.dataSource = self
         
     }
     
-    private func setupSetting() {
-        
-    }
+    
+}
+
+extension CCVideoSearchController {
+    
     
     // MARK: - UITableView Datasource
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -62,22 +65,10 @@ class CCVideoSearchController: UITableViewController,UISearchBarDelegate,UISearc
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
+        let cell = tableView.dequeueReusableCellWithIdentifier(String(UITableViewCell), forIndexPath: indexPath)
         if searchController.active {
-            
-            var cell = tableView.dequeueReusableCellWithIdentifier(identifier)
-            
-            if cell == nil {
-                
-                cell = UITableViewCell(style: .Default, reuseIdentifier: identifier)
-            }
-            
-            let model: CCVideoModel = dataSource[indexPath.row]
-            
-            cell!.textLabel?.text = model.name
-            
-            return cell!
+            cell.textLabel?.text = dataSource[indexPath.row].name!
         }
-        
         return UITableViewCell()
         
     }
@@ -94,34 +85,43 @@ class CCVideoSearchController: UITableViewController,UISearchBarDelegate,UISearc
         
         let cell = tableView.cellForRowAtIndexPath(indexPath)
         cell?.selected = false
-
-        let player = CCPlayerViewController(mediaModel: (dataSource[indexPath.row] as? CCVideoModel)!)
         
-        dispatch_async(dispatch_get_main_queue()) { 
+        let player = CCPlayerViewController(mediaModel: (dataSource[indexPath.row]))
+        
+        dispatch_async(dispatch_get_main_queue()) {
             
             self.searchController.presentViewController(player, animated: true, completion: nil)
         }
         
     }
     
+}
+
+extension CCVideoSearchController: UISearchBarDelegate,UISearchResultsUpdating,UISearchControllerDelegate {
+    
     // MARK: - UISearchController
     func updateSearchResultsForSearchController(searchController: UISearchController) {
-//        dispatch_async(dispatch_get_main_queue()) { 
-//            self.resultVC.tableView.reloadData()
-//        }
+        //        dispatch_async(dispatch_get_main_queue()) {
+        //            self.resultVC.tableView.reloadData()
+        //        }
+    }
+    
+    func didPresentSearchController(searchController: UISearchController) {
+        searchController.searchResultsController?.view.layoutIfNeeded()
     }
     
     
     // MARK: - UISearchBar Delegate
+    
     func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
         
         CCNetService.fetchSearchResult(searchText, success: { [unowned self] (models) in
             self.dataSource = models
             self.resultVC.tableView.reloadData()
-            }) { (msg) in
-                
+        }) { (msg) in
+            
         }
     }
     
-    
+
 }
